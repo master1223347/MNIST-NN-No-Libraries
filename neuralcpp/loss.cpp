9001fd87f2287d92342280e77572f7649e12f54f
master1223347 <epicgamer43798@gmail.com>
2026-01-12 12:59:53 +0530

    update MSE loss function

--- neuralcpp/loss.cpp
+++ neuralcpp/loss.cpp
@@ -1,27 +1,29 @@
 #include "loss.h"
 
+//Mean Squared Error forward pass
 double mse_forward(
     const double* y,
     const double* t,
     int size
 ) {
-    double loss = 0.0;
+    double loss = 0.0; //store error
 
-    for (int i = 0; i < size; i++) {
-        double diff = y[i] - t[i];
-        loss += diff * diff;
+    for (int i = 0; i < size; i++) { //looping over all output neurons
+        double diff = y[i] - t[i]; //calculates diff between model pred and actual answer
+        loss += diff * diff; //squares diffs
     }
 
-    return loss / size;
+    return loss / size; //avg error
 }
 
+//Mean Squared Error backward pass
 void mse_backward(
     const double* y,
     const double* t,
     double* grad_y,
     int size
 ) {
-    for (int i = 0; i < size; i++) {
-        grad_y[i] = 2.0 * (y[i] - t[i]) / size;
+    for (int i = 0; i < size; i++) { //loops over all output neurons
+        grad_y[i] = 2.0 * (y[i] - t[i]) / size; //loss gradient
     }
 }
